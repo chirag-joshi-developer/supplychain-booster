1d3ae4385174b35158351a8ef121f043c749bad4
chirag <chiragjoshi723@gmail.com>
2026-08-18 01:43:19 +0530

    fixing things

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:////app/data/value_chain.db"
+# Docker sets DATABASE_URL to use /app/data/; local dev falls back to ./value_chain.db
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./value_chain.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
